89399fbba0770d434be8682caf314017e986f20d
Jayakumar Jayaraman <jay@jayakumars-imac.home>
2017-09-25 22:16:00 +0100

    simple router example added

--- route/src/app/app.module.ts
+++ route/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Lesson1Component } from './lesson1/lesson1.component';
+import { Lesson2Component } from './lesson2/lesson2.component';
+
+export const routes: Routes = [
+  {path : 'lesson1', component : Lesson1Component},
+  {path : 'lesson2', component : Lesson2Component}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lesson1Component,
+    Lesson2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash : true})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
