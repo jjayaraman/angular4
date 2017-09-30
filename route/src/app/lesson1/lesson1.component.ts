9878bba921480cda087c23c7cee3ce0ee6258faf
Jayakumar Jayaraman <jay@jayakumars-imac.home>
2017-09-30 14:20:42 +0100

    rest api with param

--- route/src/app/lesson1/lesson1.component.ts
+++ route/src/app/lesson1/lesson1.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
-/** import { Map } from 'rxjs/add/operator/map';  **/
+
 @Component({
   selector: 'app-lesson1',
   templateUrl: './lesson1.component.html',
@@ -9,11 +9,14 @@ import { UserService } from '../services/user.service';
 export class Lesson1Component implements OnInit {
 
   users: User[];
-
+  user: User;
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.userService.getUsers().subscribe(data => {this.users = data; });
+    this.userService.getUsers().subscribe(data => { this.users = data; });
+    this.userService.getUser().subscribe(data2 => { this.user = data2; });
+
+    console.log('user :: ' + this.users);
   }
 
 
--- route/src/app/services/user.service.ts
+++ route/src/app/services/user.service.ts
@@ -3,12 +3,19 @@ import { Injectable } from '@angular/core';
 import { User } from '../lesson1/lesson1.component';
 import 'rxjs/add/operator/map';
 
+const REST_URL = 'http://localhost:3000';
+
 @Injectable()
 export class UserService {
 
   constructor(private http: Http) { }
 
+
   getUsers() {
-     return this.http.get('http://localhost:3000/users').map(res => res.json());
-    }
+    return this.http.get(REST_URL + '/users').map(res => res.json());
+  }
+
+  getUser() {
+    return this.http.get(REST_URL + '/users').map(res => res.json());
+  }
 }
